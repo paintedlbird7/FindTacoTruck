6508f22727be94e7807010c248f52111dfbf451c
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-30 20:10:18 -0800

    "Renders homepage and About page"

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // GET
-router.get('', (req, res) => {  
-    res.render('index')
+router.get('', (req, res) => {
+    const locals = {
+        title: "Find Taco Truck",
+        description: "Find Taco Truck created with NodeJs, Express & MongoDb."
+      }  
+    res.render('index', { locals })
+})
+
+router.get('/about', (req, res) => {  
+    res.render('about')
 })
 
 module.exports = router;
